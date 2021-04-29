255348cc1ee2b1c702e38c374cf8e0909714b9cb
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2021-04-29 15:56:20 -0700

    Layout, and _app.js language change wrapper

---
+++ components/Layout.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head"
+import styles from "../styles/Layout.module.css"
+import Link from "next/link"
+
+export default function Layout({children,title = "National-Filter"}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
+import React,{useState,useEffect,useCallback,useMemo} from 'react'
 import '../styles/globals.css'
 
+
+export const LangContext = React.createContext({
+  language: 'en',
+  switchLanguage:() =>{}
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [language,setlanguage] = useState("en")
+
+  const switchLanguage = useCallback(function(){
+    setlanguage((l)=>(l ==="en"? "jp":"en"))
+  },[])
+
+
+  useEffect(() => {
+    document.documentElement.lang = language;
+  }, [language]);
+
+  const value = useMemo(
+    function () {
+      return {
+        language,
+        switchLanguage,
+      };
+    },
+    [language, switchLanguage]
+  );
+
+  return(
+    <LangContext.Provider value={value}>
+      <Component {...pageProps} />
+    </LangContext.Provider>
+  ) 
+
+
 }
 
 export default MyApp
